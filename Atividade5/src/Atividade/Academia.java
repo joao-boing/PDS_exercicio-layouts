78f8e8d79674fb5b101558c921e46d73820c53b1
joao-boing <joaovtboing@gmail.com>
2026-04-22 12:08:00 -0300

    Commit Da academia projeto

---
+++ Atividade5/src/Atividade/Academia.java
@@ -0,0 +1,41 @@
+package Atividade;
+
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import net.miginfocom.swing.MigLayout;
+
+public class Academia extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Academia frame = new Academia();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Academia() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		contentPane.setLayout(new MigLayout("", "[]", "[]"));
+
+	}
+
+}
